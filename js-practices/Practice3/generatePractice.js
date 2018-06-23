2ae8d97f8e73d81affb9ca444790d033ca8fc4d5
reijiokito <romeothecong@gmail.com>
2018-06-23 16:15:15 +0700

    Update file

--- js-practices/Practice3/generatePractice.js
+++ js-practices/Practice3/generatePractice.js
@@ -5,10 +5,10 @@ function generate(testLengthArray){
   return testLengthArray.map((item,index) => {
     var a = {
       input: (() => {
-        arr = [];        
+        let arr = [];        
         for(let i=0;i<item;i++)
           arr.push(Math.floor((Math.random() * 100) + 1));
-          arr.sort(input.sort((a,b) => a-b));
+          arr.sort(((a,b) => a-b));
           return arr;
       }) (),                       //run function
       target: 0,
@@ -21,7 +21,7 @@ function generate(testLengthArray){
       if(testLengthArray.length>=4){
         if(index==0)
           target = a.input[0];
-        else if (index=item-1){
+        else if (index==item-1){
           target = a.input[testLengthArray[item-1]-1];
         }else{
           target = Math.floor((Math.random() * 100) + 1);
